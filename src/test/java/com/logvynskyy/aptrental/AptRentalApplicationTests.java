8cb1635a92df77c61d8cbc57bfb765efef5e207d
Logvynskyy <shimon511bm@gmail.com>
2022-01-20 15:38:07 +0200

    Initial commit

---
+++ src/test/java/com/logvynskyy/aptrental/AptRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logvynskyy.aptrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
